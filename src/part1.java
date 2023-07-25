d3b008ec8cfb1b1402b461e42c8d6cb719b113c3
Asu21244 <hawa21244@gmail.com>
2023-07-24 20:35:51 -0400

    new pt1 so far

--- src/part1.java
+++ src/part1.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -5,5 +6,11 @@ import java.util.Scanner;
 
 public class part1 {
     public static void main(String[] args) {
+        BufferedReader buffer=new BufferedReader(new FileRead("Part1_input_file_names.txt"));
+        String line=null;
+        line=buffer.readLine();// Number of files to read
+        int i=0;
+        
+
     }
 }
