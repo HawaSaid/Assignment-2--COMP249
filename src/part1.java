5ddd4c6c34ad8d63e5675f9a730ec4dc9fd0747b
Asu21244 <hawa21244@gmail.com>
2023-07-24 20:29:23 -0400

    restarting pt1

--- src/part1.java
+++ src/part1.java
@@ -5,20 +5,5 @@ import java.util.Scanner;
 
 public class part1 {
     public static void main(String[] args) {
-        try {
-            FileReader reader = new FileReader("Part1_input_file_names.txt");
-            int i = reader.read();
-            while (i != -1) {
-                i=reader.read();
-                reader.close();
-
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        catch(IOException e){
-            e.printStackTrace();
-
-        }
     }
 }
