fce3af1756b5c89f13fb51d7d1218b42e87c80e2
Asu21244 <hawa21244@gmail.com>
2023-07-25 22:19:59 -0400

    blablabla

--- src/part1.java
+++ src/part1.java
@@ -1,17 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.File;
 
 public class part1 {
     public static void main(String[] args) {
 
         // Try-catch block to handle IO exception
         try {
-            // Creates an instance of the BufferedReader class
-            BufferedReader buffer = new BufferedReader(new FileReader("Part1_input_file_names.txt"));
-
+            BufferedReader buffer = new BufferedReader(new FileReader("Part1_input_file_names.txt"));// Creates an instance of the BufferedReader class
             String line = null;
-
             line = buffer.readLine();// Contains the files to read
             int numberofLines = 0;
             int i = 0;
@@ -23,26 +21,31 @@ public class part1 {
             }
             // Loop to read the txt files containing the CSV files
             while ((line = buffer.readLine()) != null) {
-                
+
                 // Array of type String created to store the CSV files in the txt file
-                
+
                 readFile = new String[numberofLines];
                 readFile[i] = line;// Content of file stored in readFile array
                 i++;
-                
             }
             buffer.close();
 
-            for(int j=0;j<readFile.length;j++){
+            for (int j = 0; j < readFile.length; j++) {
+                    try{
+                    FileReader reader=new FileReader(readFile[j]);
+                    int files=reader.read();
+                    while(files!=-1){
+                        System.out.println("Here is the content of the file you want to read: "+);
+                    }
+                    reader.close();
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
 
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch(IOException e){
+        e.printStackTrace();
         }
-
-
-
     }
+
 }
