683766db3368545b2cabb44e09651f5996d509f3
Asu21244 <hawa21244@gmail.com>
2023-07-25 21:37:08 -0400

    tanner

--- src/part1.java
+++ src/part1.java
@@ -15,6 +15,7 @@ public class part1 {
             line = buffer.readLine();// Contains the files to read
             int numberofLines = 0;
             int i = 0;
+            String[] readFile;
 
             // Loop to find the amount of lines we have in the txt
             while ((line = buffer.readLine()) != null) {
@@ -25,15 +26,23 @@ public class part1 {
                 
                 // Array of type String created to store the CSV files in the txt file
                 
-                String[] readFile = new String[numberofLines];
+                readFile = new String[numberofLines];
                 readFile[i] = line;// Content of file stored in readFile array
                 i++;
+                
             }
             buffer.close();
+
+            for(int j=0;j<readFile.length;j++){
+
+
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
 
     }
 }
